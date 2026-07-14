d3a53e7d84e338623e1c0f9649b30b008590eb7b
Dalon <italonso@hotmail.com>
2026-07-13 23:28:33 -0500

    Conexion a Azure PostgreSQL

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,20 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('redplus_db', 'postgres', '', {
-  host: 'localhost',
+
+const sequelize = new Sequelize('postgres', 'admin_redplus', 'aJa3%1K^$3%P0&04', {
+  
+  host: 'redplus-db-grupo-6.postgres.database.azure.com', 
+  
   dialect: 'postgres',
   port: 5432,       
-  logging: false,   //Para no ver tantos mensajes
+  logging: false,
+  
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 export default sequelize;
